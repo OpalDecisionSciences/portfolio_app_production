dc127772d64c06bef12b5722baa0f3d1605eb243
Jeannine Jordan <iamai@Mac.lan>
2025-08-06 00:47:33 -0400

    Fix EC2 IP address in deployment script to use Elastic IP

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -12,7 +12,7 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Configuration
-EC2_IP="13.223.94.223"
+EC2_IP="34.233.24.183"
 KEY_FILE="~/.ssh/opal-decision-sciences-prod-kp.pem"
 EC2_USER="ubuntu"
 APP_DIR="/home/ubuntu/portfolio_app_production"
